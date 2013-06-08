bbd33ef45936bb4b35e3f5f21f89fde0c7aada47
Earlz <earlz@lastyearswishes.com>
2013-06-08 19:22:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metastories::Application.config.secret_token = 'ae00d0322c3c441888e607685fb510f091ba3e46200ab11de737e5b7db28401e647b14157ab4e2d10a2c42e3229830df26d5b7b2268f41dd48a148bc3795ed9e'
