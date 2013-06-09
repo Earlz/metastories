66b817edd58bccf5193dd04ab3faae798291d1d2
Earlz <earlz@lastyearswishes.com>
2013-06-08 20:06:18 -0400

    unit testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Metastories::Application.routes.draw do
-  get "home/index"
+  resources :stories
 
+
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -4,4 +4,24 @@ class StoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    test "should not save story without title" do
+        story = Story.new
+        story.name="foo";
+        story.content="meh";
+        assert !story.save
+    end
+    test "should not save story with short title" do
+        s = Story.new
+        s.name="foo"
+        s.content="meh"
+        s.title="f"
+        assert !s.save
+    end
+    test "should not save story without name" do
+        s=Story.new
+        s.title="foobar"
+        s.content="mehhhh"
+        assert !s.save
+    end
+    
 end
