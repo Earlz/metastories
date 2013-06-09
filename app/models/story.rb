730231ad9a0ec097e9df101d6c9f8d3820bf004a
Earlz <earlz@lastyearswishes.com>
2013-06-08 20:08:21 -0400

    standard 2-space tabs

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,5 +2,5 @@ class Story < ActiveRecord::Base
   attr_accessible :content, :name, :title
   validates :name, :presence => true
   validates :title, :presence => true,
-             :length => {:minimum => 5}
+      :length => {:minimum => 5}
 end
